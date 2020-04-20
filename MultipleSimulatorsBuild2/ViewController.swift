f453109519f367c2984c5470242233e318e9ea9f
Rafael Lucena <rafael.lucena@arctouch.com>
2020-04-20 02:44:35 -0300

    Adjust project

---
+++ MultipleSimulatorsBuild2/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MultipleSimulatorsBuild
+//
+//  Created by Rafael Lucena on 4/19/20.
+//  Copyright © 2020 Rafael Lucena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
